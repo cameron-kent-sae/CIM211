eee6442828e0db680d9712bf46af4308aa4ff028
cameron-kent-sae <1020666@student.sae.edu.au>
2021-07-29 12:35:11 +1000

    Player leveling

--- Project 2501/Assets/Scripts/PlayerEvolutionManager.cs
+++ Project 2501/Assets/Scripts/PlayerEvolutionManager.cs
@@ -10,9 +10,23 @@ public class PlayerEvolutionManager : MonoBehaviour
     public int allegiantCount;
     public int divergentCount;
     public int insurgentCount;
+
+    public int allegiantLevel;
+    public int divergentLevel;
+    public int InsurgentLevel;
+
+    //public int[] evoTargets = {0, 2, 5, 10, 17, 26, 37, 50, 65, 82, 101};
+    public List<int> evoTargets = new List<int> { 2, 5, 10, 17, 26, 37, 50, 65, 82, 101 };
     #endregion
 
     #region Methods
+    private void Start()
+    {
+        allegiantCount = 0;
+        divergentCount = 0;
+        insurgentCount = 0;
+    }
+
     private void OnApplicationQuit()
     {
         dataInventory.Container.Clear();    
@@ -27,6 +41,7 @@ public class PlayerEvolutionManager : MonoBehaviour
             {
                 case DataType.Allegiant:
                     allegiantCount += 1;
+                    CheckAllegiantLevel();
                     break;
                 case DataType.Divegent:
                     divergentCount += 1;
@@ -41,6 +56,20 @@ public class PlayerEvolutionManager : MonoBehaviour
             Destroy(other.gameObject);
             Debug.Log(data.data.description);
         }
+    }
+
+    private void CheckAllegiantLevel()
+    {
+        //if a typeCount == an evoTarget -> update player appearance
+        //for (int i = 0; i < evoTargets.Length; i++)
+        foreach (int target in evoTargets)
+        {
+            if (allegiantCount == target)
+            {
+                allegiantLevel = evoTargets.IndexOf(target);
+            }
+        }
+        Debug.Log(allegiantLevel);
     }
     #endregion
 }
