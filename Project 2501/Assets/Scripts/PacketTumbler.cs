f23636bea7727c38b676ff92e11e028e257a1a06
cameron-kent-sae <1020666@student.sae.edu.au>
2021-08-05 13:56:06 +1000

    Packet Tumbler

---
+++ Project 2501/Assets/Scripts/PacketTumbler.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PacketTumbler : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(BeginObjectRotate());
+    }
+
+    private IEnumerator BeginObjectRotate()
+    {
+        while (true)
+        {
+            float xSpin = Random.Range(0, 360);
+            float ySpin = Random.Range(0, 360);
+            float zSpin = Random.Range(0, 360);
+            float spinTime = Random.Range(.3f, .9f);
+
+            transform.LeanRotateX(xSpin, spinTime).setEaseInOutBounce();
+            transform.LeanRotateY(xSpin, spinTime).setEaseInOutBounce();
+            transform.LeanRotateZ(xSpin, spinTime).setEaseInOutBounce();
+            //transform.rotation = Quaternion.Euler(xSpin, ySpin, zSpin);
+
+            yield return new WaitForSeconds(spinTime);
+        }
+    }
+}
