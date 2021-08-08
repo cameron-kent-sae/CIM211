0f81635a228ec83712f37e95699fbdb5d82eaf66
cameron-kent-sae <1020666@student.sae.edu.au>
2021-08-08 11:54:00 +1000

    UI prototyping

---
+++ Project 2501/Assets/Scripts/DataLogger.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DataLogger : MonoBehaviour
+{
+    #region Variables
+    public int maxLInes = 16;
+    public TMP_Text textLog;
+
+    public GameObject logPanel;
+
+    private List<string> eventLog = new List<string>();
+    private string guiText = "";
+
+    private bool isExpanded;
+
+    #endregion
+
+    #region Built In Methods
+    private void Start()
+    {
+        textLog.text = guiText;
+        isExpanded = false;
+    }
+
+    private void Update()
+    {
+        if (isExpanded)
+        {
+            if (Input.GetButtonDown("Tab"))
+            {
+                logPanel.LeanMoveX(-175f, .2f).setEaseOutBounce();
+                isExpanded = false;
+            }
+        }
+        else if (!isExpanded)
+        {
+            if (Input.GetButtonDown("Tab"))
+            {
+                logPanel.LeanMoveX(270f, .2f).setEaseInBounce();
+                isExpanded = true;
+            }
+        }
+    }
+    #endregion
+
+    #region Custom Methods
+    public void AddLog(string newDataLog)
+    {
+        eventLog.Add(newDataLog);
+
+        if (eventLog.Count >= maxLInes)
+        {
+            eventLog.RemoveAt(0);
+        }
+
+        guiText = "";
+
+        foreach (string log in eventLog)
+        {
+            guiText += log;
+            guiText += "\n";
+        }
+
+        textLog.text = guiText;
+    }
+    #endregion
+}
