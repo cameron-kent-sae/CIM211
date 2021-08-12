07d240bf88eaf649c12a8a3f3f231b4b1f8ff9a4
cameron-kent-sae <1020666@student.sae.edu.au>
2021-08-12 16:23:16 +1000

    Intro work

--- Project 2501/Assets/Scripts/ButtonPulser.cs
+++ Project 2501/Assets/Scripts/ButtonPulser.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ButtonPulser : MonoBehaviour
 {
-    private float approachSpeed = .3f;
-    private float growthBound = 1.1f;
-    private float shrinkBound = 0.8f;
+    private float approachSpeed = .1f;
+    private float growthBound = 1f;
+    private float shrinkBound = 0.9f;
     private float currentRatio = 1;
     public bool keepGoing = true;
 
@@ -22,31 +22,20 @@ public class ButtonPulser : MonoBehaviour
 
     private IEnumerator Pulse()
     {
-        // Run this indefinitely
         while (keepGoing)
         {
-            // Get bigger for a few seconds
             while (this.currentRatio != this.growthBound)
             {
-                // Determine the new ratio to use
                 currentRatio = Mathf.MoveTowards(currentRatio, growthBound, approachSpeed * Time.deltaTime);
-
-                // Update our text element
                 this.transform.localScale = Vector3.one * currentRatio;
-                //this.icon.text = "Growing!";
 
                 yield return new WaitForEndOfFrame();
             }
 
-            // Shrink for a few seconds
             while (this.currentRatio != this.shrinkBound)
             {
-                // Determine the new ratio to use
                 currentRatio = Mathf.MoveTowards(currentRatio, shrinkBound, approachSpeed * Time.deltaTime);
-
-                // Update our text element
                 this.transform.localScale = Vector3.one * currentRatio;
-                //this.icon.text = "Shrinking!";
 
                 yield return new WaitForEndOfFrame();
             }
